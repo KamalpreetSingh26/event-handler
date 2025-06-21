9f10f473cc53f59afaf71c306662507d0e5e70fb
KamalpreetSingh26 <saini.kamalpreet26@gmail.com>
2025-06-21 20:55:45 +0530

    Added logic for accounts app

--- event_handler/manage.py
+++ event_handler/manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_handler.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
